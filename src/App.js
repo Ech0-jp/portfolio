277d5f55f1126499b433d3e312b732106694cb0e
Ech0-jp <hit-the-hills@hotmail.ca>
2017-08-17 15:19:59 -0400

    implemented rendering of Entry and Main

--- src/App.js
+++ src/App.js
@@ -1,21 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Entry from './pages/entry/Entry';
+import Main from './pages/main/Main';
+import MatrixCanvas from './util/component/MatrixCanvas';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(){
+        super();
+        this.state = {next: null, showNext: false};
+    }
+
+    componentDidMount(){
+        var hash = window.location.hash;
+        switch (hash) {
+            case '#main':
+                this.setState({current: 'main'});
+                break;
+            default:
+                this.setState({current: 'entry'});
+        }
+    }
+
+    page(renderNew = false){
+        const statement = renderNew ? this.state.next : this.state.current;
+        switch (statement) {
+            case 'entry':
+                return <Entry changePage={this.changePage.bind(this)}/>;
+            case 'main':
+                return <Main loaded={this.changePageTransition.bind(this)} startInterval={!renderNew} />;
+            default:
+                return <noscript />;
+        }
+    }
+
+    changePage(next, direction){
+        this.setState({next: next, transition: true});
+        this.direction = direction;
+    }
+
+    changePageTransition(){
+        if (this.refs.transitionCanvas)
+            this.refs.transitionCanvas.transitionReverse();
+    }
+
+    changePageResolve(){
+        this.setState({
+            current: this.state.next,
+            next: null,
+            showNext: false,
+            transition: false
+        });
+    }
+
+    transition(){
+        if (!this.state.transition) return;
+        return <MatrixCanvas ref="transitionCanvas" style={{zIndex: 9999}} transition={this.direction} callback={() => {this.setState({showNext: true})}} resolve={this.changePageResolve.bind(this)} />;
+    }
+
+    render() {
+        var next = <noscript />;
+        if (this.state.next && this.state.showNext) {
+            next = this.page(true);
+        }
+        return (
+            <div className="App">
+                {this.page()}
+                {next}
+                {this.transition()}
+            </div>
+        );
+    }
 }
 
 export default App;
