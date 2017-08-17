936b559d850fdb005d0bfae643b3542a20b6a78b
Ech0-jp <hit-the-hills@hotmail.ca>
2017-08-17 15:22:04 -0400

    impleneted animated border button

---
+++ src/util/component/BorderButton.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import '../style/BorderTransition.css';
+
+var classNames = require('classnames');
+
+class BorderButton extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { animate: false, active: false };
+        this.timeout = this.props.timeout ? this.props.timeout : 25;
+    }
+
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({animate: true});
+        }, this.timeout);
+    }
+
+    setActive(){
+        this.setState({active: true});
+    }
+
+    DOM(){
+        return this.instance;
+    }
+
+    render(){
+        var classes = classNames(
+            this.props.className,
+            'border', {
+            'border-transition': this.state.animate,
+            'active': this.state.active
+        });
+
+        return (
+            <button id={this.props.id} className={classes}
+                onClick={this.props.onClick} ref={(instance) => { this.instance = instance }}>
+                {this.props.content}
+            </button>
+        );
+    }
+}
+
+export default BorderButton;
