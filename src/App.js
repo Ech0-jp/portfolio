bc9ce3a1832d9a4dcef42aecc004ae634fd2ffe9
Ech0-jp <hit-the-hills@hotmail.ca>
2017-08-21 16:27:24 -0400

    page can now be determined by href and added contact

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 import './App.css';
 import Entry from './pages/entry/Entry';
 import Main from './pages/main/Main';
+// import Projects from './pages/projects/Projects';
+import Contact from './pages/contact/Contact';
+// import About from './pages/about/About';
+// import Resume from './pages/resume/Resume';
 import MatrixCanvas from './util/component/MatrixCanvas';
 
 class App extends Component {
@@ -16,32 +20,28 @@ class App extends Component {
         switch (hash) {
             case '#main':
                 this.setState({current: 'main'});
-                break;
+            break;
+            case '#projects':
+                this.setState({current: 'projects'});
+            break;
+            case '#contact':
+                this.setState({current: 'contact'});
+            break;
+            case '#about':
+                this.setState({current: 'about'});
+            break;
+            case '#resume':
+                this.setState({current: 'resume'});
+            break;
             default:
                 this.setState({current: 'entry'});
         }
     }
 
-    page(renderNew = false){
-        const statement = renderNew ? this.state.next : this.state.current;
-        switch (statement) {
-            case 'entry':
-                return <Entry changePage={this.changePage.bind(this)}/>;
-            case 'main':
-                return <Main loaded={this.changePageTransition.bind(this)} startInterval={!renderNew} />;
-            default:
-                return <noscript />;
-        }
-    }
 
     changePage(next, direction){
         this.setState({next: next, transition: true});
         this.direction = direction;
-    }
-
-    changePageTransition(){
-        if (this.refs.transitionCanvas)
-            this.refs.transitionCanvas.transitionReverse();
     }
 
     changePageResolve(){
@@ -53,9 +53,34 @@ class App extends Component {
         });
     }
 
+    changePageTransition(){
+        if (this.refs.transitionCanvas)
+            this.refs.transitionCanvas.transitionReverse();
+    }
+
+    page(renderNew = false){
+        const statement = renderNew ? this.state.next : this.state.current;
+        switch (statement) {
+            case 'entry':
+                return <Entry changePage={this.changePage.bind(this)}/>;
+            case 'main':
+                return <Main loaded={this.changePageTransition.bind(this)} changePage={this.changePage.bind(this)} transitionComplete={!renderNew} />;
+            case 'projects':
+                return <noscript />;
+            case 'contact':
+                return <Contact loaded={this.changePageTransition.bind(this)} changePage={this.changePage.bind(this)} transitionComplete={!renderNew} />;
+            case 'about':
+                return <noscript />;
+            case 'resume':
+                return <noscript />;
+            default:
+                return <noscript />;
+        }
+    }
+
     transition(){
         if (!this.state.transition) return;
-        return <MatrixCanvas ref="transitionCanvas" style={{zIndex: 9999}} transition={this.direction} callback={() => {this.setState({showNext: true})}} resolve={this.changePageResolve.bind(this)} />;
+        return <MatrixCanvas ref="transitionCanvas" style={{zIndex: 9999}} transition={this.direction} intervalTime={10} callback={() => {this.setState({showNext: true})}} resolve={this.changePageResolve.bind(this)} />;
     }
 
     render() {
