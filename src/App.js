07dbf6a2479c73db90551088b362fdead048118f
Ech0-jp <hit-the-hills@hotmail.ca>
2017-08-22 18:08:27 -0400

    app will now transition to all pages.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 import './App.css';
 import Entry from './pages/entry/Entry';
 import Main from './pages/main/Main';
-// import Projects from './pages/projects/Projects';
+import Projects from './pages/projects/Projects';
 import Contact from './pages/contact/Contact';
-// import About from './pages/about/About';
-// import Resume from './pages/resume/Resume';
+import About from './pages/about/About';
+import Resume from './pages/resume/Resume';
 import MatrixCanvas from './util/component/MatrixCanvas';
 
 class App extends Component {
@@ -66,13 +66,13 @@ class App extends Component {
             case 'main':
                 return <Main loaded={this.changePageTransition.bind(this)} changePage={this.changePage.bind(this)} transitionComplete={!renderNew} />;
             case 'projects':
-                return <noscript />;
+                return <Projects loaded={this.changePageTransition.bind(this)} changePage={this.changePage.bind(this)} transitionComplete={!renderNew} />;
             case 'contact':
                 return <Contact loaded={this.changePageTransition.bind(this)} changePage={this.changePage.bind(this)} transitionComplete={!renderNew} />;
             case 'about':
-                return <noscript />;
+                return <About loaded={this.changePageTransition.bind(this)} changePage={this.changePage.bind(this)} transitionComplete={!renderNew} />;
             case 'resume':
-                return <noscript />;
+                return <Resume loaded={this.changePageTransition.bind(this)} changePage={this.changePage.bind(this)} transitionComplete={!renderNew} />;
             default:
                 return <noscript />;
         }
@@ -84,14 +84,13 @@ class App extends Component {
     }
 
     render() {
-        var next = <noscript />;
+        var page = this.page();
         if (this.state.next && this.state.showNext) {
-            next = this.page(true);
+            page = this.page(true);
         }
         return (
             <div className="App">
-                {this.page()}
-                {next}
+                {page}
                 {this.transition()}
             </div>
         );
