53303b9ec2b3f37f4c39f3005cfc10a67c6e3d92
Ech0-jp <hit-the-hills@hotmail.ca>
2017-10-28 08:33:26 -0400

    Swapped the positions of the Cover Letter and Resume.

--- src/pages/resume/Resume.js
+++ src/pages/resume/Resume.js
@@ -10,8 +10,8 @@ class Resume extends Component {
         super(props);
         this.state = {
             transitionComplete: props.transitionComplete,
-            resumeOpen: true,
-            coverLetterOpen: false
+            coverLetterOpen: true,
+            resumeOpen: false
         };
     }
 
@@ -78,14 +78,14 @@ class Resume extends Component {
         return (
             <div className="resume-container">
                 <ExpandingTextButton ref="header" className="header" style={style} rotate="-90" content="Resume" targetWidth={width} onClick={() => this.onClick()} />
-                <PDFContainer ref="resumeContainer" className="resume-pdf-container"
-                    buttonContent="Resume" buttonAlign="LEFT" btnWidth={144} btnHeight={50}
-                    width={720} height={750} onClick={() => { this.onPdfClick('resume') }}
-                   open={true} btnContent="Resume" content={require('../../pdf/AndrewGray_Resume_Example.pdf')} />
                 <PDFContainer ref="cvContainer" className="cv-pdf-container"
-                    buttonContent="Cover Letter" buttonAlign="RIGHT" btnWidth={144} btnHeight={50}
-                    width={720} height={750} onClick={() => { this.onPdfClick('cover-letter') }}
+                    buttonContent="Cover Letter" buttonAlign="LEFT" btnWidth={144} btnHeight={50}
+                    width={720} height={750} open={true} onClick={() => { this.onPdfClick('cover-letter') }}
                    btnContent="Cover Letter" content={require('../../pdf/AndrewGray_CoverLetter_Example.pdf')} />
+                   <PDFContainer ref="resumeContainer" className="resume-pdf-container"
+                       buttonContent="Resume" buttonAlign="RIGHT" btnWidth={144} btnHeight={50}
+                       width={720} height={750} onClick={() => { this.onPdfClick('resume') }}
+                       btnContent="Resume" content={require('../../pdf/AndrewGray_Resume_Example.pdf')} />
             </div>
         );
     }
